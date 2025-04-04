7a339379e86ba3cdd3f427d467dcfd3f83c1f6ea
Aleksandr Harisov <aharisov@smbindustries.fr>
2025-04-04 15:59:37 +0200

    added base operations

---
+++ index.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+import router from "./src/routes/index.js"
+import bodyParser from "body-parser";
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(router);
+app.listen(3000);
---
+++ src/controllers/calc-controller.ts
@@ -0,0 +1,94 @@
+import { Request, Response } from "express";
+import CalcService from "../services/calc-service.js";
+
+const calcService = new CalcService();
+
+export const getAddRes = async (req: Request, res: Response) => {
+    try {
+        const { num1, num2 } = req.body;
+
+        if (isNaN(num1) || isNaN(num2)) {
+            return res.status(400).json({
+                error: "Des nombres invalides.",
+            });
+        }
+
+        const addRes = calcService.getAddRes(num1, num2);
+
+        res.json({
+            text: 'La somme est égale',
+            value: addRes
+        });
+    } catch (e: any) {
+        res.status(500).json({ error: e.message });
+    }
+};
+
+export const getSubRes = async (req: Request, res: Response) => {
+    try {
+        const { num1, num2 } = req.body;
+
+        if (isNaN(num1) || isNaN(num2)) {
+            return res.status(400).json({
+                error: "Des nombres invalides.",
+            });
+        }
+
+        const subRes = calcService.getSubRes(num1, num2);
+
+        res.json({
+            text: 'La différence est égale',
+            value: subRes
+        });
+    } catch (e: any) {
+        res.status(500).json({ error: e.message });
+    }
+};
+
+export const getMultRes = async (req: Request, res: Response) => {
+    try {
+        const { num1, num2 } = req.body;
+
+        if (isNaN(num1) || isNaN(num2)) {
+            return res.status(400).json({
+                error: "Des nombres invalides.",
+            });
+        }
+
+        const multRes = calcService.getMultRes(num1, num2);
+
+        res.json({
+            text: 'Le produit est égale',
+            value: multRes
+        });
+    } catch (e: any) {
+        res.status(500).json({ error: e.message });
+    }
+};
+
+export const getDivRes = async (req: Request, res: Response) => {
+    try {
+        const { num1, num2 } = req.body;
+
+        if (isNaN(num1) || isNaN(num2)) {
+            return res.status(400).json({
+                error: "Des nombres invalides.",
+            });
+        }
+
+        if (num2 == 0) {
+            return res.status(500).json({
+                error: "La division par zero est interdit.",
+            });
+        }
+
+        const divRes = calcService.getDivRes(num1, num2);
+
+        res.json({
+            text: 'La division est égale',
+            value: divRes
+        });
+    } catch (e: any) {
+        res.status(500).json({ error: e.message });
+    }
+};
